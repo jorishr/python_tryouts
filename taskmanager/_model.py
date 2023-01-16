67d8836c6a01951bf7ca2ed50db86372ed96050f
jorishr <joris@liondigits.com>
2023-01-16 17:34:16 +0100

    Refactor: grouped getter and setter functions;added: class function that returns all properties in list

--- taskmanager/_model.py
+++ taskmanager/_model.py
@@ -11,23 +11,26 @@ class Task:
     def getName(self):
         return self.name
 
-    def setName(self, name):
-        self.name = name
+    def getDeadline(self):
+        return self.deadline
 
     def getPriority(self):
         return self.priority
     
-    def setPriority(self, priority):
-        self.priority = priority
-    
-    def getDeadline(self):
-        return self.deadline
+    def getAll(self):
+        return [self.name,self.deadline,self.priority]
+
+    def setName(self, name):
+        self.name = name   
     
     def setDeadline(self, deadline):
         self.deadline = deadline
+
+    def setPriority(self, priority):
+        self.priority = priority
 
 task1 = Task('work', 'today', 'low')
 #print(task1.getName())
 #print(task1.getPriority())
 #task1.setPriority('high')
-#print(task1.getPriority())
+#print(task1.getAll())
